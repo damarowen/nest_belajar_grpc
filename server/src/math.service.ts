89a41f26eb4e9c477a382ccbe48f3f88159164bc
damarowen <damarowen@gmailcom>
2022-12-19 22:41:23 +0700

    init

---
+++ server/src/math.service.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MathService {
+    public accumulate(data: number[]): number {
+        return (data || []).reduce((a, b) => Number(a) + Number(b));
+    }
+}
